d35c2b1f42660d887c78428257fcfbbfda8cbe01
Nipa Das Gupta <89456649+NipaDasGupta@users.noreply.github.com>
2022-05-03 16:26:24 +0800

    Add files via upload

---
+++ pink_morsel_test.py
@@ -0,0 +1,11 @@
+from JupyterDash App-1 import app
+
+
+def test_header_exists(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#header", timeout=10)
+
+
+def test_visualization_exists(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#visualization", timeout=10)
